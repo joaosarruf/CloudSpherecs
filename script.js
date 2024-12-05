631b2ec80d3b580dba8e55f96f0491b7b3475182
Joao Pedro Henderson Sarruf <48034139+joaosarruf@users.noreply.github.com>
2024-12-05 20:00:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Menu responsivo (opcional)
+const nav = document.querySelector('header nav');
+const toggle = document.createElement('div');
+toggle.classList.add('nav-toggle');
+toggle.innerHTML = '<i class="fas fa-bars"></i>';
+document.querySelector('header .container').appendChild(toggle);
+
+toggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
